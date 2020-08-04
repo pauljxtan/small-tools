76bb384712d2ac8c401af3622b271a301f8f65fe
Paul Tan <pjxtan@gmail.com>
2020-08-04 11:02:50 -0400

    Handle both X and Wayland

--- file-to-clipboard.sh
+++ file-to-clipboard.sh
@@ -2,4 +2,13 @@
 
 # Copies the content of a file onto the system clipboard
 
-xclip -sel cli < "$1"
+if [ "$XDG_SESSION_TYPE" = "x11" ]
+then
+    xclip -sel cli < "$1"
+elif [ "$XDG_SESSION_TYPE" = "wayland" ]
+then
+    wl-copy < "$1"
+else
+    echo "Unrecognized XDG_SESSION_TYPE: $XDG_SESSION_TYPE"
+    echo "Expected x11 or wayland"
+fi
